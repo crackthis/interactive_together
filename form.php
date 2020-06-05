4cad22f30be5411001ecb9b2d2a8184faca6947d
crackthis <prm1078life@gmail.com>
2020-06-05 21:56:56 +0200

    IntTogether upload

---
+++ form.php
@@ -0,0 +1,31 @@
+<?php
+	if ( !empty($_POST) && trim($_POST['name']) != '' && trim($_POST['email']) != '' && trim($_POST['message']) != ''  ) {
+
+		$message =  "Site: \n" .
+					"Name: " . $_POST['name'] . "\n" .
+					"Email: ". $_POST['email'] . "\n" .
+					"Number:". $_POST['number'] . "\n" .
+					"Message: ". $_POST['message'];
+	
+		mail( 'samirasabulis@gmail.com', "Interactive Together", $message ); 
+		$new_url = 'return.html';
+		header('Location: '.$new_url);
+		
+	}
+	
+	
+	
+	function checkValue($item, $message ) {
+		if ( isset($item) && trim($item) == ''  ) {
+			echo '<div class="error">' . $message . '</div>';
+		} 
+	}
+	
+	function printPostValue($item){
+		if ( isset($item) && strlen(trim($item)) > 0 ) {
+			echo $item;
+		}
+	}
+
+
+?>
---
+++ js/script.js
@@ -0,0 +1,96 @@
+$(".modal").each( function(){
+	$(this).wrap('<div class="overlay"></div>');
+});
+
+$(".open-modal").on('click', function(e){
+	e.preventDefault();
+	e.stopImmediatePropagation;
+	
+	var $this = $(this),
+			modal = $($this).data("modal");
+	
+	$(modal).parents(".overlay").addClass("open");
+	setTimeout( function(){
+		$(modal).addClass("open");
+	}, 350);
+	
+	$(document).on('click', function(e){
+		var target = $(e.target);
+		
+		if ($(target).hasClass("overlay")){
+			$(target).find(".modal").each( function(){
+				$(this).removeClass("open");
+			});
+			setTimeout( function(){
+				$(target).removeClass("open");
+			}, 350);
+		}
+		
+	});
+	
+});
+
+$(".close-modal").on('click', function(e){
+	e.preventDefault();
+	e.stopImmediatePropagation;
+	var $this = $(this),
+			modal = $($this).data("modal");
+	
+	$(modal).removeClass("open");
+	setTimeout( function(){	
+		$(modal).parents(".overlay").removeClass("open");
+	}, 350);
+	
+});
+
+$(window).scroll(function(){
+    if ($(window).scrollTop() > 10) {
+        $('.header-menu').addClass('scroll');
+    }
+    else {
+        $('.header-menu').removeClass('scroll');
+    }
+});
+
+$(document).ready(function(){
+	$("#slow_scroll").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+});
+
+$(document).ready(function(){
+	$("#slow_scroll_btn").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+});
+
+$(document).ready(function(){
+	$("#slow_burger").on("click","a", function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 1500);
+		
+	});
+	
+});
